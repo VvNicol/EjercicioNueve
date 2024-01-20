c9656e24f7639f7f8491c5b4528072b8790e053d
VvNicol <150023181+VvNicol@users.noreply.github.com>
2024-01-20 16:07:05 +0100

    Add files via upload

---
+++ edu.ejercicio.nueve.nrojlla/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace edu.ejercicio.nueve.nrojlla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un array de números enteros del 1 al 8
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            // Crear un ArrayList con las provincias de Andalucía
+            ArrayList provinciasAndalucia = new ArrayList()
+            {
+             "Almería", "Cádiz", "Córdoba", "Sevilla" ,"Granada", "Huelva", "Jaén", "Málaga"
+            };
+
+            provinciasAndalucia.Sort();
+
+            provinciasAndalucia.Insert(0, numeros[0]);
+        
+            for(int i = 1; i < numeros.Length; i++)
+            {
+                provinciasAndalucia.Insert(i * 2, numeros[i]);
+            }
+
+            foreach(var andalucia  in provinciasAndalucia)
+            {
+                Console.WriteLine(andalucia);
+            }
+
+        }
+    }
+}
